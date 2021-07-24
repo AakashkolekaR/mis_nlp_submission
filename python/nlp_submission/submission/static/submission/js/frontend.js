9afc96867bfcdfa7ccb54a8ca715e237c451b062
Aakash <aakashkolekar98@gmail.com>
2021-07-24 19:32:53 +0530

    comments added

--- python/nlp_submission/submission/static/submission/js/frontend.js
+++ python/nlp_submission/submission/static/submission/js/frontend.js
@@ -1,6 +1,7 @@
 function validateForm() {
     let sentence = document.forms["nlpForm"]["sent"].value;
     if (sentence ===  ""){
-        alert("Please enter a sentence")
+        alert("Please enter a sentence");
+        return false
     }
 }
