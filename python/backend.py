b4209512756a4f5918f16e79c6fc60ab95ce9d5e
Aakash <aakashkolekar98@gmail.com>
2021-07-25 20:53:51 +0530

    added comments

--- python/backend.py
+++ python/backend.py
@@ -1,3 +1,4 @@
+#imported all the packages
 import nltk
 from nltk.corpus import stopwords
 from nltk.corpus.reader import lin
@@ -5,8 +6,11 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem import WordNetLemmatizer
 stop_words = set(stopwords.words('english'))
 
+#name of the input  and output file
 input_file = 'backend_test.txt'
+output_file = 'submission_output.txt'
 
+#readlines from the file buffer
 with open(input_file) as file:
     lines   =  file.readlines()
     lines = [line.split("\n")[0] for line in lines ]
@@ -18,14 +22,17 @@ tagged_sent_nouns = 0
 tagged_list = []
 lemma_list = []
 
+# initialized the lemmatizer
 lemmatizer = WordNetLemmatizer()
 
-for line in lines:	
+for line in lines:
+    # performed word tokenization
     word_list = nltk.word_tokenize(line)
-    # total_words_with_stop_words += len(word_list)
 
+    #removed stopwords
     word_list = [word for word in word_list if not word in stop_words]
 
+    #parts of speech tagging and lemmatization for a sentence 
     tagged = nltk.pos_tag(word_list)
     for tag in tagged:
         if len(tag[1]) > 1:
@@ -37,7 +44,8 @@ for line in lines:
     tagged_list.append(tagged)
 
 
-with open('submission_output.txt', 'w') as f:
+#writing the result to the output file
+with open(output_file, 'w') as f:
     print('==========', file=f)
     print("Total no of sentences are",len(lines), file=f)
     print('==========', file=f)
